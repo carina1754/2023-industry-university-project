93a52effbe0502cae0097e7fcedcc1c19a8853dc
GyuYoung Lee <gylee0409@yonsei.ac.kr>
2023-06-13 03:17:14 +0900

    top5 헤비유저를 쳐낸 데이터 확보

--- WhoIsHeavyUser/New_reservation.py
+++ WhoIsHeavyUser/New_reservation.py
@@ -3,15 +3,30 @@ import pandas as pd
 # Load the DataFrame
 df = pd.read_csv('df_reservation.csv')
 
-# Read the top 5 duplicated dog names from the txt file
-with open('top_5_duplicated_dog_names.txt', 'r') as file:
-    top_5_names = [line.strip() for line in file]
+# Create a new column by combining 'dogName' and 'dogSize'
+df['dogIdentity'] = df['dogName'] + df['dogSize']
 
-# Remove all rows where the dogName is in top_5_names
+# Read the top 5 duplicated dog names and sizes from the txt file
+top_5_names = []
+with open('top_5_duplicated_dog_names.txt', 'r') as file:
+    for line in file:
+        # Split the line at the ':', and get only the dog's name
+        name, _ = line.split(":")
+        # If there is a comma in the name, split again to get the individual names
+        if "," in name:
+            names = name.split(",")
+            top_5_names.extend(names)
+        else:
+            top_5_names.append(name)
+
+# Trim leading/trailing white space from the names
+top_5_names = [name.strip() for name in top_5_names]
+
+# Remove all rows where the 'dogName' is in top_5_names
 df = df[~df['dogName'].isin(top_5_names)]
 
 # Print the total number of remaining cases
 print(f"Total number of remaining cases: {len(df)}")
 
 # Write the remaining data to a new csv file
-df.to_csv('filtered_def_reservation.csv', index=False)
+df.to_csv('filtered_df_reservation.csv', index=False)
